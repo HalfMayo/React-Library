5c9c9128693991defe7247955c1972d04c9bea2b
Half_Barba <barbara.sandrolini@gmail.com>
2023-08-02 19:18:23 +0200

    old storybook-static removed

--- storybook-static/assets/blank-picture-frame-by-houseplant-corner-parquet-floor-bb6fc28f.js
+++
@@ -1,2 +0,0 @@
-const r=""+new URL("blank-picture-frame-by-houseplant-corner-parquet-floor-50c1bc4b.jpg",import.meta.url).href;export{r as p};
-//# sourceMappingURL=blank-picture-frame-by-houseplant-corner-parquet-floor-bb6fc28f.js.map
